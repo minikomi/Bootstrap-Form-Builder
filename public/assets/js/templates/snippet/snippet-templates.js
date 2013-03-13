aa74e91b837c0314f9f07070af2cbcea309d5a0e
Adam Moore <nerdfunk@gmail.com>
2013-03-13 22:01:49 +0900

    clean up templates

--- public/assets/js/templates/snippet/snippet-templates.js
+++ public/assets/js/templates/snippet/snippet-templates.js
@@ -1,14 +1,44 @@
-define( function(require) {
-  var formname = require('text!app/templates/snippet/formname.html')
-  , prepend = require('text!app/templates/snippet/prependedtext.html')
-  , search = require('text!app/templates/snippet/searchinput.html')
-  , textinput = require('text!templates/snippet/textinput.html');
+define(function(require) {
+  var formname               = require('text!templates/snippet/formname.html')
+  , prependedtext            = require('text!templates/snippet/prependedtext.html')
+  , search                   = require('text!templates/snippet/searchinput.html')
+  , textinput                = require('text!templates/snippet/textinput.html');
+  , appendedcheckbox         = require('text!templates/snippet/appendedcheckbox.html')
+  , appendedtext             = require('text!templates/snippet/appendedtext.html')
+  , button                   = require('text!templates/snippet/button.html')
+  , formname                 = require('text!templates/snippet/formname.html')
+  , multiplecheckboxes       = require('text!templates/snippet/multiplecheckboxes.html')
+  , multiplecheckboxesinline = require('text!templates/snippet/multiplecheckboxesinline.html')
+  , multipleradios           = require('text!templates/snippet/multipleradios.html')
+  , multipleradiosinline     = require('text!templates/snippet/multipleradiosinline.html')
+  , prependedcheckbox        = require('text!templates/snippet/prependedcheckbox.html')
+  , prependedtext            = require('text!templates/snippet/prependedtext.html')
+  , searchinput              = require('text!templates/snippet/searchinput.html')
+  , selectbasic              = require('text!templates/snippet/selectbasic.html')
+  , selectmultiple           = require('text!templates/snippet/selectmultiple.html')
+  , snippet-templates        = require('text!templates/snippet/snippet-templates.js')
+  , textarea                 = require('text!templates/snippet/textarea.html')
+  , textinput                = require('text!templates/snippet/textinput.html');
 
   return {
-    formname: formname,
-    prependedtext: prepend,
-    searchinput: search,
-    textinput: textinput
+    formname                   : formname
+    , prependedtext            : prependedtext
+    , search                   : search
+    , textinput                : textinput
+    , appendedcheckbox         : appendedcheckbox
+    , appendedtext             : appendedtext
+    , button                   : button
+    , formname                 : formname
+    , multiplecheckboxes       : multiplecheckboxes
+    , multiplecheckboxesinline : multiplecheckboxesinline
+    , multipleradios           : multipleradios
+    , multipleradiosinline     : multipleradiosinline
+    , prependedcheckbox        : prependedcheckbox
+    , prependedtext            : prependedtext
+    , searchinput              : searchinput
+    , selectbasic              : selectbasic
+    , selectmultiple           : selectmultiple
+    , textarea                 : textarea
+    , textinput                : textinput
   }
 });
-
