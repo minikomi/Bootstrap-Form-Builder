0eee60cb96574960ca6f6166183a163f3a65037e
Adam Moore <nerdfunk@gmail.com>
2013-03-19 14:23:43 +0900

    optimize

--- public/assets/js/views/tab-snippet.js
+++ public/assets/js/views/tab-snippet.js
@@ -15,11 +15,11 @@ define([
     }
     , mouseDownHandler: function(mouseDownEvent){
       mouseDownEvent.preventDefault();
+      mouseDownEvent.stopPropagation();
       //hide all popovers
       $(".popover").hide();
       $("body").append(new TempSnippetView({model: new SnippetModel($.extend(true,{},this.model.attributes))}).render());
       PubSub.trigger("newTempPostRender", mouseDownEvent);
-      return false;
     }
   });
 });
