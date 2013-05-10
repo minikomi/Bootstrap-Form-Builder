58f7120bc0ffd1adb38ba6491104ca106fc1f581
Adam Moore <nerdfunk@gmail.com>
2013-05-10 12:30:44 +0900

    Add size selector for inputs

--- assets/js/models/snippet.js
+++ assets/js/models/snippet.js
@@ -4,7 +4,11 @@ define([
   return Backbone.Model.extend({
     getValues: function(){
       return _.reduce(this.get("fields"), function(o, v, k){
-        o[k]  = v["value"];
+        if (v["type"] == "select") {
+          o[k] = _.find(v["value"], function(o){return o.selected})["value"];
+        } else {
+          o[k]  = v["value"];
+        }
         return o;
       }, {});
     }
