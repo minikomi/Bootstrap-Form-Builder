cd460924e9ecfee3961784d01c250e4334d5272b
Moore Adam <adam@stdsgn5.semitra.com>
2013-10-09 11:21:51 +0900

    easier this way.

--- assets/js/data/parse.rb
+++ assets/js/data/parse.rb
@@ -7,6 +7,8 @@ require "yaml"
 yamlfiles = Dir.glob("#{ File.dirname(__FILE__) }/*.yaml")
 
 yamlfiles.each{ |file|
+
+  puts(file)
   yaml = YAML.load_file(file)
   json = JSON.pretty_generate(YAML.load_file(file))
   File.open(file.gsub("\.yaml", "\.json"), 'w'){ |out| puts "writing #{out.path}"; out.write(json) }
