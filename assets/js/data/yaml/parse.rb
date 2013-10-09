576fb9a823f3feeef3b42641ba75d1cfd7b7235c
Moore Adam <adam@stdsgn5.semitra.com>
2013-10-09 11:17:03 +0900

    fix ruby script

--- assets/js/data/yaml/parse.rb
+++ assets/js/data/yaml/parse.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require "rubygems"
 require "json"
 require "yaml"
 
@@ -8,5 +9,5 @@ yamlfiles = Dir.glob("#{ File.dirname(__FILE__) }/*.yaml")
 yamlfiles.each{ |file|
   yaml = YAML.load_file(file)
   json = JSON.pretty_generate(YAML.load_file(file))
-  File.open("../"+file.gsub("\.yaml", "\.json"), 'w'){ |out| puts "writing #{out.path}"; out.write(json) }
+  File.open(file.gsub("\.yaml", "\.json"), 'w'){ |out| puts "writing #{out.path}"; out.write(json) }
 }
